df3e0486772353814b62560fb1e977e94876fd37
Roberto Grijalva Sanchez <roberd.999117@gmail.com>
2023-08-27 19:57:51 -0600

    Android Calculator Project

---
+++ index.js
@@ -0,0 +1,35 @@
+const result = document.querySelector('.result');
+const buttons = document.querySelectorAll('.button');
+
+for(let i=0;i<buttons.length;i++){
+    buttons[i].addEventListener('click',(e)=>{
+        let data = e.target.getAttribute('data');
+        switch(data){
+            case "C":
+                result.innerText="";
+                break;
+            case "=":
+                try{
+                    result.innerText = eval(result.innerText);
+                }catch{
+                    result.innerText = "Error";
+                }
+                break;
+            case "back":
+                if(result.innerText){
+                    result.innerText = result.innerText.slice(0,-1); 
+                }
+                break;
+            case "+/-":
+                result.innerText = - result.innerText;
+                break;
+            case "%":
+                if(result.innerText){
+                    result.innerText /= 100;
+                }
+                break;
+            default:
+                result.innerText+=data;
+        }
+    })
+}
